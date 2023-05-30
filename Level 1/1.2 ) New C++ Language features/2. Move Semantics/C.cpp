6d44a6294996f778477a5800ee3911407a87a45e
Marcus Guerrand <57366572+marcusguerranda@users.noreply.github.com>
2023-05-29 23:08:39 -0400

    Create C.cpp

---
+++ Level 1/1.2 ) New C++ Language features/2. Move Semantics/C.cpp
@@ -0,0 +1,25 @@
+#include "C.hpp"
+
+#ifndef C_cpp
+#define C_cpp
+
+//CONSTRUCTORS AND DESTRUCTOR
+
+    //Assignment operator
+    template <typename T>
+    C<T>& C<T>::operator = (const C& source) noexcept
+    {
+        m_vector = source.m_vector;
+        std::cout << "Assignment operator in C class has been used." << std::endl;
+        return *this;
+    };
+
+    //Destructor
+    template <typename T>
+    C<T>::~C() noexcept 
+    {
+        std::cout << "Destructor in C class has been used." << std::endl;
+    };                      
+
+
+#endif //C_cpp
